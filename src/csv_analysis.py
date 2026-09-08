98b4ef21126057b373750dc4a60df11644efe5cd
Fiona1926 <fiona19206@gmail.com>
2026-09-08 14:05:16 +0800

    del csv_analysis.py

--- src/csv_analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_1= pd.read_csv("/Users/fionaleong/cargo_flight_monitor/past_data/current_flights_2026-07-27.csv")
-df_filtered_1=df_1[df_1['airline']=='CPA']
-dest_list=df_filtered_1['destination'].tolist()
-df_filtered_2=df_1[(df_1['airline']!='CPA') & (df_1['destination'].isin(dest_list))]
-print(df_filtered_1)
-print(df_filtered_2)
